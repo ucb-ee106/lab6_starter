1664ad1916cdf8572673227cac54608dd3559590
Jaiveer Singh <j.singh@berkeley.edu>
2021-10-04 19:53:22 -0700

    Update comment to explicitly suggest int64 for overflow

--- segmentation/src/image_segmentation.py
+++ segmentation/src/image_segmentation.py
@@ -150,7 +150,7 @@ def edge_detect_naive(gray_img):
     G_y = cv2.filter2D(None, -1, None)
 
     # Finally, compute G = sqrt(Gx ** 2 + Gy ** 2)
-    # Use np.sqrt() and np.pow() for overflow safety!
+    # If you have errors computing the square root, you may be encountering overflow - change datatype to int64
     G = None
 
     # Return G
